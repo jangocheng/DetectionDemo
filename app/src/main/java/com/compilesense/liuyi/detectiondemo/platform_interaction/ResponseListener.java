1f8ba477c2e3bd49f3177bcb64ef6181e4dcad06
glasssix <jaryjun@glasssix.com>
2016-10-10 17:45:29 +0800

    完善人脸检测和分析4个API

--- app/src/main/java/com/compilesense/liuyi/detectiondemo/platform_interaction/ResponseListener.java
+++
@@ -1,9 +0,0 @@
-package com.compilesense.liuyi.detectiondemo.platform_interaction;
-
-/**
- * Created by shenjingyuan002 on 16/9/5.
- */
-public interface ResponseListener {
-    void success(String response);
-    void failed();
-}
